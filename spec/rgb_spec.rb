47b7b6c0a32f5b9ea164ae8e8411da7f2caa54af
harutoreon <norabder567@gmail.com>
2023-07-08 02:27:49 +0900

    add: Add rgb convert spec code

--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -1,16 +1,26 @@
-require 'minitest/autorun'
 require './lib/rgb'
 
-class RgbTest < Minitest::Test
-  def test_to_hex
-    assert_equal '#000000', to_hex(0, 0, 0)
-    assert_equal '#ffffff', to_hex(255, 255, 255)
-    assert_equal '#043c78', to_hex(4, 60, 120)
+RSpec.describe 'RGB変換プログラム' do
+  describe '10進数を16進数に変換' do
+    it '#000000は(0, 0, 0)に変換されること' do
+      expect('#000000').to eq to_hex(0, 0, 0) 
+    end
+    it '#ffffffは(255, 255, 255)に変換されること' do
+      expect('#ffffff').to eq to_hex(255, 255, 255)
+    end
+    it '043c78は(4, 60, 120)に変換されること' do
+      expect('#043c78').to eq to_hex(4, 60, 120)
+    end
   end
-
-  def test_to_ints
-    assert_equal [0, 0, 0], to_ints('#000000')
-    assert_equal [255, 255, 255], to_ints('#ffffff')
-    assert_equal [4, 60, 120], to_ints('#043c78')
+  describe '16進数を10進数に変換' do
+    it '[0, 0, 0]は#000000に変換されること' do
+      expect([0, 0, 0]).to eq to_ints('#000000')
+    end
+    it '[255, 255, 255]は#ffffffに変換されること' do
+      expect([255, 255, 255]).to eq to_ints('#ffffff')
+    end
+    it '[4, 60, 120]は#043c78に変換されること' do
+      expect([4, 60, 120]).to eq to_ints('#043c78')
+    end
   end
 end
