01f1adbab36da4c197e76fbb46b23ba195dfe8d7
harutoreon <norabder567@gmail.com>
2023-07-09 05:41:21 +0900

    test: Created ticket gate spec

--- spec/gate_spec.rb
+++ spec/gate_spec.rb
@@ -1,38 +1,38 @@
 require './lib/gate'
 require './lib/ticket'
 
-RSpec.xdescribe 'ticket gate spec' do
-  
+RSpec.describe 'ticket gate spec' do
+  before do
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)    
+  end
+
+  example 'when get on at Umeda and get off at Juso' do
+    ticket = Ticket.new(150)
+    @umeda.enter(ticket)
+
+    expect(@juso.exit(ticket)).to eq true
+  end
+
+  example 'when get on at Umeda and get off at mikuni' do
+    ticket = Ticket.new(190)
+    @umeda.enter(ticket)
+
+    expect(@mikuni.exit(ticket)).to eq true
+  end
+
+  example 'when get on at Juso and get off at Mikuni' do
+    ticket = Ticket.new(150)
+    @juso.enter(ticket)
+
+    expect(@mikuni.exit(ticket)).to eq true
+  end
+
+  example 'Failure: when get on at Umeda and get off at mikuni' do
+    ticket = Ticket.new(150)
+    @umeda.enter(ticket)
+
+    expect(@mikuni.exit(ticket)).to eq false
+  end
 end
-
-# class GateTest < Minitest::Test
-#   def setup
-#     @umeda = Gate.new(:umeda)
-#     @juso = Gate.new(:juso)
-#     @mikuni = Gate.new(:mikuni)
-#   end
-
-#   def test_umeda_to_juso
-#     ticket = Ticket.new(150)
-#     @umeda.enter(ticket)
-#     assert @juso.exit(ticket)
-#   end
-
-#   def test_umeda_to_mikuni_when_fare_is_not_enough
-#     ticket = Ticket.new(150)
-#     @umeda.enter(ticket)
-#     refute @mikuni.exit(ticket)
-#   end
-
-#   def test_umeda_to_mikuni_when_fare_is_enough
-#     ticket = Ticket.new(190)
-#     @umeda.enter(ticket)
-#     assert @mikuni.exit(ticket)
-#   end
-
-#   def test_juso_to_mikuni
-#     ticket = Ticket.new(150)
-#     @juso.enter(ticket)
-#     assert @mikuni.exit(ticket)
-#   end
-# end
