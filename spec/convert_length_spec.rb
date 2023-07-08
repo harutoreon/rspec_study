2778fbad4e1fd733bd64415fdacdfc47d0bed4ee
harutoreon <norabder567@gmail.com>
2023-07-08 10:31:52 +0900

    style: length convert spec

--- spec/convert_length_spec.rb
+++ spec/convert_length_spec.rb
@@ -2,12 +2,12 @@ require './lib/convert_length'
 
 RSpec.describe 'length convert spec' do
   it 'returns 39.37' do
-    expect(convert_length(1, from: :m, to: :in)).to eq(39.37)
+    expect(convert_length(1, from: :m, to: :in)).to eq 39.37
   end
   it 'returns 0.38' do
-    expect(convert_length(15, from: :in, to: :m)).to eq(0.38)
+    expect(convert_length(15, from: :in, to: :m)).to eq 0.38
   end
   it 'returns 10670.73' do
-    expect(convert_length(35000, from: :ft, to: :m)).to eq(10670.73)
+    expect(convert_length(35000, from: :ft, to: :m)).to eq 10670.73
   end
 end
