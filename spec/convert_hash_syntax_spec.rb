192d48296dc7cc334e1e8cd1116733dc13acaa8c
harutoreon <norabder567@gmail.com>
2023-07-08 15:06:40 +0900

    refactor: Introduction let syntax

--- spec/convert_hash_syntax_spec.rb
+++ spec/convert_hash_syntax_spec.rb
@@ -1,24 +1,26 @@
 require './lib/convert_hash_syntax'
 
 RSpec.describe 'convert hash syntax spec' do
-  before do
-    @old_syntax = <<~TEXT
-      {
-        :name => 'Alice',
-        :age => 20,
-        :gender => :female
-      }
+  let(:old_syntax) {
+    text = <<~TEXT
+    {
+      :name => 'Alice',
+      :age => 20,
+      :gender => :female
+    }
     TEXT
-    @new_syntax = <<~TEXT
+  }
+  let(:new_syntax) {
+    text = <<~TEXT
       {
         name: 'Alice',
         age: 20,
         gender: :female
       }
     TEXT
-  end
+  }
 
   it 'returns new hash syntax' do
-    expect(convert_hash_syntax(@old_syntax)).to eq @new_syntax
+    expect(convert_hash_syntax(old_syntax)).to eq new_syntax
   end
 end
