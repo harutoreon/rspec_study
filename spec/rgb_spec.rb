805035855e2e76cebec3d9079d600885caf9cc12
harutoreon <norabder567@gmail.com>
2023-07-08 09:20:34 +0900

    fix: Fix expectation syntax

--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -1,26 +1,26 @@
 require './lib/rgb'
 
 RSpec.describe 'RGB convert' do
-  describe 'Convert ints to hex' do
-    it 'returns (0, 0, 0)' do
-      expect('#000000').to eq to_hex(0, 0, 0) 
+  context 'Convert ints to hex' do
+    it 'returns #000000' do
+      expect(to_hex(0, 0, 0)).to eq '#000000' 
     end
-    it 'returns (255, 255, 255)' do
-      expect('#ffffff').to eq to_hex(255, 255, 255)
+    it 'returns #ffffff' do
+      expect(to_hex(255, 255, 255)).to eq '#ffffff'
     end
-    it 'returns (4, 60, 120)' do
-      expect('#043c78').to eq to_hex(4, 60, 120)
+    it 'returns #043c78' do
+      expect(to_hex(4, 60, 120)).to eq '#043c78'
     end
   end
-  describe 'Convert hex to ints' do
-    it 'returns #000000' do
-      expect([0, 0, 0]).to eq to_ints('#000000')
+  context 'Convert hex to ints' do
+    it 'returns [0, 0, 0]' do
+      expect(to_ints('#000000')).to eq [0, 0, 0]
     end
     it 'returns [255, 255, 255]' do
-      expect([255, 255, 255]).to eq to_ints('#ffffff')
+      expect(to_ints('#ffffff')).to eq [255, 255, 255]
     end
     it 'returns [4, 60, 120]' do
-      expect([4, 60, 120]).to eq to_ints('#043c78')
+      expect(to_ints('#043c78')).to eq [4, 60, 120]
     end
   end
 end
