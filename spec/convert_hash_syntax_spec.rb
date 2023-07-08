959bfc592c6fbca691c8669f388b17dbd133d148
harutoreon <norabder567@gmail.com>
2023-07-08 13:28:45 +0900

    test: Created convert hash syntax spec

--- spec/convert_hash_syntax_spec.rb
+++ spec/convert_hash_syntax_spec.rb
@@ -1,29 +1,22 @@
-# require 'minitest/autorun'
 require './lib/convert_hash_syntax'
 
-RSpec.xdescribe 'convert hash syntax spec' do
-  it 'does something' do
-    
+RSpec.describe 'convert hash syntax spec' do
+  it 'returns new hash syntax' do
+    old_syntax = <<~TEXT
+      {
+        :name => 'Alice',
+        :age => 20,
+        :gender => :female
+      }
+    TEXT
+    new_syntax = <<~TEXT
+      {
+        name: 'Alice',
+        age: 20,
+        gender: :female
+      }
+    TEXT
+
+    expect(convert_hash_syntax(old_syntax)).to eq new_syntax
   end
 end
-
-# class ConvertHashSyntaxTest < Minitest::Test
-#   def test_convert_hash_syntax
-#     old_syntax = <<~TEXT
-#       {
-#         :name => 'Alice',
-#         :age => 20,
-#         :gender => :female
-#       }
-#     TEXT
-#     expected = <<~TEXT
-#       {
-#         name: 'Alice',
-#         age: 20,
-#         gender: :female
-#       }
-#     TEXT
-
-#     assert_equal expected, convert_hash_syntax(old_syntax)
-#   end
-# end
