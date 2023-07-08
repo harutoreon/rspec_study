7dce718de536c85bcbb967f4b0b2097d49f6cc22
harutoreon <norabder567@gmail.com>
2023-07-08 10:15:42 +0900

    add: Add lib and spec file

---
+++ spec/convert_length_spec.rb
@@ -0,0 +1,13 @@
+require './lib/convert_length'
+
+RSpec.xdescribe 'length convert spec' do
+  it 'does something' do
+    # assert_equal 39.37, convert_length(1, from: :m, to: :in)  
+  end
+  it 'does something' do
+    # assert_equal 0.38, convert_length(15, from: :in, to: :m)
+  end
+  it 'does something' do
+    # assert_equal 10670.73, convert_length(35000, from: :ft, to: :m)
+  end
+end
